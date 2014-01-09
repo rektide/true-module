235663d7eafe89fa1671f01d7545c2115d5b7cbd
rektide <rektide@voodoowarez.com>
2014-01-09 01:04:35 -0500

    Capture requesting module in true-module to allow proper resolving.

--- true-module.js
+++ true-module.js
@@ -1,21 +1,35 @@
 var fs= require("fs"),
+  path= require("path"),
   resolve= require("resolve")
 
-module.exports= TrueModule
-
-/**
-  True-Module
-*/
-
-function TrueModule(name){
-	var self= this||TrueModule
-	console.log("i",require("util").inspect(self))
-	var filename= self.resolver(name)
-	var contents= self.read(filename)
-	var val= self.eval(contents)
-	console.log("*",filename,contents,val)
-	return val
+function resolveSync(name){
+	return resolve.sync(name,this)
 }
-TrueModule.resolver= resolve.sync
-TrueModule.read= fs.readFileSync
-TrueModule.eval= eval
+
+function readSync(filename){
+	return "("+fs.readFileSync(filename,"utf8")+")"
+}
+
+
+module.exports= function(module){
+	function TrueModule(name){
+		var self= this==global?TrueModule:this,
+		  val
+		try{
+			var filename= self.resolver(name)
+			var contents= self.read(filename)
+			console.log("%",filename,contents)
+			val= self.eval(contents)
+			console.log("%",filename,contents,val)
+		}catch(ex){
+			console.log("death",ex)
+			throw ex
+		}
+		return val
+	}
+	TrueModule.basedir= path.dirname(module.filename)
+	TrueModule.resolver= resolveSync
+	TrueModule.read= readSync
+	TrueModule.eval= eval
+	return TrueModule
+}
